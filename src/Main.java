3efa93b7d1966379d3557b748893580a2223745d
faxcon08 <faxcon-08@mail.ru>
2022-11-03 17:28:28 +0300

    Поменяли вывод и определили переменную.

--- src/Main.java
+++ src/Main.java
@@ -1,9 +1,6 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
-        System.out.println("Вторая попытка");
-        System.out.println("Some test");
-        System.out.println("Внес изменения");
-        System.out.println("Changing 2");
+        System.out.println("Привет, Мир!");
+        int st = 10;
     }
 }
